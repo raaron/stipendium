49ca2ec9ba7cde5346db32dc28d03f0968e03e33
Aaron Richiger <a.richi@bluewin.ch>
2013-10-09 23:47:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stipendium::Application.config.secret_token = 'da02581e19141623800cd22ffd5d60e23d7d3d3a8598d49135ea73d70bb97664101cc450bd5c3bc79d9df29251861a15d0c10306c59af2c670be8096d3be8797'
